e02ca11b45769c9e265c80da25965d84ba7113f7
EdinsonRequena <edinsonrequena@protonmail.com>
2020-03-29 01:03:36 -0400

    adding snake body

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 
 
 import tkinter as tk
-
+from PIL import Image, ImageTk
 
 class SnakeGame(tk.Canvas):
     ''' game settings '''
 
     def __init__(self):
         super().__init__(width = 600, height = 620, background = 'salmon1', highlightthickness = 0 )
+
+        self.snake_positions = [(100, 100), (80, 100), (60, 100)]
+
+
+        self.assets()
+        self.create_objects()
+
+
+    def assets(self):
+        try:
+            self.snake_png = Image.open('./assets/snake.png')
+            self.snake_body = ImageTk.PhotoImage(self.snake_png)
+
+            self.food_png = Image.open('./assets/food.png')
+            self.food = ImageTk.PhotoImage(self.food_png)
+        except IOError as error:
+            root.destroy()
+            raise
+
+
+    def create_objects(self):
+        for x_position, y_position in self.snake_positions:
+            self.create_image(x_position, y_position, image = self.snake_body, tag = 'test')
+
 
 
 class SnakeApp:
